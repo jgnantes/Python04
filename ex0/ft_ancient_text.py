d6e7d5145d48857bc87d33edd9828b9ac8d2176b
jgnantes <jgnantes@outlook.com>
2026-02-18 14:49:25 -0300

    ex0 feito

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def ancient_text(file_name: str):
+    """"""
+    try:
+        file = open(file_name, 'r')
+        text: str = file.read()
+        file.close()
+    except (FileNotFoundError, PermissionError) as e:
+        print(f"A {type(e).__name__} occured: '{e}'")
+        print(f"The file {file_name} could no be found or accessed")
+        return
+
+    print(f"Accesing Storage Vault: {file_name}")
+    print("Connection established...\n")
+    print(f"{text}\n")
+    print("Data recovery complete. Storage unit disconnected.")
+
+
+if __name__ == "__main__":
+    print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n")
+    ancient_text('ancint_fragment.txt')
